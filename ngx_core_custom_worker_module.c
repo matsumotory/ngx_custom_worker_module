21b350793ce2c9e4612e9356607d3c3336099e8e
Masahiro Nagano <kazeburo@gmail.com>
2020-07-23 22:53:52 +0900

    add worker_process_spare

--- ngx_core_custom_worker_module.c
+++ ngx_core_custom_worker_module.c
@@ -3,12 +3,13 @@
 #include <nginx.h>
 
 static char *ngx_worker_process_factor(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static char *ngx_worker_process_spare(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_command_t ngx_core_custom_worker_commands[] = {
-
     {ngx_string("worker_processes_factor"), NGX_MAIN_CONF | NGX_DIRECT_CONF | NGX_CONF_TAKE1, ngx_worker_process_factor,
      0, 0, NULL},
-
+    {ngx_string("worker_process_spare"), NGX_MAIN_CONF | NGX_DIRECT_CONF | NGX_CONF_TAKE1, ngx_worker_process_spare, 0,
+     0, NULL},
     ngx_null_command};
 
 static ngx_core_module_t ngx_core_custom_worker_module_ctx = {ngx_string("core_custom_worker"), NULL, NULL};
@@ -46,6 +47,32 @@ static char *ngx_worker_process_factor(ngx_conf_t *cf, ngx_command_t *cmd, void 
   }
 
   ccf->worker_processes = ccf->worker_processes * factor;
+
+  return NGX_CONF_OK;
+}
+
+static char *ngx_worker_process_spare(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+  ngx_str_t *value;
+  ngx_int_t spare;
+  ngx_core_conf_t *ccf;
+
+  ccf = (ngx_core_conf_t *)ngx_get_conf(cf->cycle->conf_ctx, ngx_core_module);
+
+  if (ccf->worker_processes == NGX_CONF_UNSET) {
+    return "don't set worker process";
+  }
+
+  value = cf->args->elts;
+  spare = ngx_atoi(value[1].data, value[1].len);
+
+  if (ccf->worker_processes - spare < 1) {
+    ngx_log_error(NGX_LOG_WARN, cf->log, 0,
+                  "worker_processes is smaller than ngx_worker_process_spare. worker_processes is set to be 1");
+    spare = ccf->worker_processes - 1;
+  }
+
+  ccf->worker_processes = ccf->worker_processes - spare;
 
   return NGX_CONF_OK;
 }
